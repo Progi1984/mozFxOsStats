2e9b1851a13cbf8d6ffd74a116f80fea97572a9f
Progi1984 <progi1984@gmail.com>
2015-04-24 21:04:26 +0200

    IMPROVED : Comment and Coding Styles

--- src/generateStats.php
+++ src/generateStats.php
@@ -29,80 +29,80 @@ $arrayFiles = array();
 $oDir = new DirectoryIterator($dirDataTgz);
 foreach ($oDir as $oDirItem) {
   if ($oDirItem->isDot()) {
-	continue;
+    continue;
   }
   if ($oDirItem->getExtension() != 'tgz') {
-	continue;
+    continue;
   }
   $arrayFiles[] = pathinfo($oDirItem->getFilename(), PATHINFO_FILENAME);
 }
 asort($arrayFiles);
 
 $arrayStats = array(
-	'numApps' => array(),
-	'rating' => array(
-		'num' => array(),
-		'amount' => array(),
-		'average' => array(),
-	),
-	'stateDisabled' => array(
-		0 => array(),
-		1 => array(),
-	),
-	'statePackaged' => array(
-		0 => array(),
-		1 => array(),
-	),
-	'statePublicStats' => array(
-		0 => array(),
-		1 => array(),
-	),
-	'typeApp' => array(
-		'hosted' => array(),
-		'packaged' => array(),
-		'privileged' => array(),
-	),
-	'typeDevice' => array(
-		'android-mobile' => array(),
-		'android-tablet' => array(),
-		'desktop' => array(),
-		'firefoxos' => array(),
-	),
-	'typePremium' => array(
-		'free-inapp' => array(),
-		'free' => array(),
-		'premium' => array(),
-		'premium-inapp' => array(),
-		'other' => array(),
-	),
-	'lastMonth' => array(
-		'avgRating0' => 0,
-		'avgRating1' => 0,
-		'avgRating2' => 0,
-		'avgRating3' => 0,
-		'avgRating4' => 0,
-		'avgRating5' => 0,
-		'createdDate' => array(),
-		'geo' => array(),
-		'priceLocale' => array(),
-		'size' =>  array(
-			'1024' => 0,
-			'2048' => 0,
-			'3072' => 0,
-			'4096' => 0,
-			'5120' => 0,
-			'6144' => 0,
-			'7168' => 0,
-			'8192' => 0,
-			'9216' => 0,
-			'10240' => 0,
-			'51200' => 0,
-			'102400' => 0,
-			'512000' => 0,
-			'1024000' => 0,
-			'5120000' => 0,
-		),
-	),
+    'numApps' => array(),
+    'rating' => array(
+        'num' => array(),
+        'amount' => array(),
+        'average' => array(),
+    ),
+    'stateDisabled' => array(
+        0 => array(),
+        1 => array(),
+    ),
+    'statePackaged' => array(
+        0 => array(),
+        1 => array(),
+    ),
+    'statePublicStats' => array(
+        0 => array(),
+        1 => array(),
+    ),
+    'typeApp' => array(
+        'hosted' => array(),
+        'packaged' => array(),
+        'privileged' => array(),
+    ),
+    'typeDevice' => array(
+        'android-mobile' => array(),
+        'android-tablet' => array(),
+        'desktop' => array(),
+        'firefoxos' => array(),
+    ),
+    'typePremium' => array(
+        'free-inapp' => array(),
+        'free' => array(),
+        'premium' => array(),
+        'premium-inapp' => array(),
+        'other' => array(),
+    ),
+    'lastMonth' => array(
+        'avgRating0' => 0,
+        'avgRating1' => 0,
+        'avgRating2' => 0,
+        'avgRating3' => 0,
+        'avgRating4' => 0,
+        'avgRating5' => 0,
+        'createdDate' => array(),
+        'geo' => array(),
+        'priceLocale' => array(),
+        'size' =>  array(
+            '1024' => 0,
+            '2048' => 0,
+            '3072' => 0,
+            '4096' => 0,
+            '5120' => 0,
+            '6144' => 0,
+            '7168' => 0,
+            '8192' => 0,
+            '9216' => 0,
+            '10240' => 0,
+            '51200' => 0,
+            '102400' => 0,
+            '512000' => 0,
+            '1024000' => 0,
+            '5120000' => 0,
+        ),
+    ),
 );
 
 /**
@@ -110,48 +110,48 @@ $arrayStats = array(
  * Country : http://jvectormap.com/maps/world/world/
  */
 $arrayGeo = array(
-	'ar' => 'AE',
-	'bg' => 'BG',
-	'bn-BD' => 'BD',
-	'ca' => 'ES',
-	'cs' => 'CZ',
-	'de' => 'DE',
-	'en-US' => 'US',
-	'el' => 'GR',
-	'es' => 'ES',
-	'eu' => 'ES',
-	'fa' => 'IR',
-	'fi' => 'FI',
-	'fr' => 'FR',
-	'hr' => 'HR',
-	'hu' => 'HU',
-	'id' => 'ID',
-	'it' => 'IT',
-	'ja' => 'JP',
-	'ko' => 'KR',
-	'nl' => 'NL',
-	'pl' => 'PL',
-	'pt-BR' => 'BR',
-	'pt-PT' => 'PT',
-	'ro' => 'RO',
-	'ru' => 'RU',
-	'sl' => 'SI',
-	'sq' => 'AL',
-	'sr' => 'RS',
-	'sv-SE' => 'SE',
-	'ta' => 'IN',
-	'tr' => 'TR',
-	'vi' => 'VN',
-	'zh-CN' => 'CN',
-	'zh-TW' => 'TW',
+    'ar' => 'AE',
+    'bg' => 'BG',
+    'bn-BD' => 'BD',
+    'ca' => 'ES',
+    'cs' => 'CZ',
+    'de' => 'DE',
+    'en-US' => 'US',
+    'el' => 'GR',
+    'es' => 'ES',
+    'eu' => 'ES',
+    'fa' => 'IR',
+    'fi' => 'FI',
+    'fr' => 'FR',
+    'hr' => 'HR',
+    'hu' => 'HU',
+    'id' => 'ID',
+    'it' => 'IT',
+    'ja' => 'JP',
+    'ko' => 'KR',
+    'nl' => 'NL',
+    'pl' => 'PL',
+    'pt-BR' => 'BR',
+    'pt-PT' => 'PT',
+    'ro' => 'RO',
+    'ru' => 'RU',
+    'sl' => 'SI',
+    'sq' => 'AL',
+    'sr' => 'RS',
+    'sv-SE' => 'SE',
+    'ta' => 'IN',
+    'tr' => 'TR',
+    'vi' => 'VN',
+    'zh-CN' => 'CN',
+    'zh-TW' => 'TW',
 );
 
 foreach($arrayFiles as $key => $filename){
-  // Création du dossier
+  // Create the directory
   if(!file_exists($dirDataTgz.$filename)){
-	mkdir($dirDataTgz.$filename);
+    mkdir($dirDataTgz.$filename);
   }
-  // Décompression
+  // Unzip
   $archiveTGZ = new PharData($dirDataTgz.$filename.'.tgz');
   $archiveTGZ->decompress();
   unset($archiveTGZ);
@@ -188,7 +188,7 @@ foreach($arrayFiles as $key => $filename){
       $bLast = true;
   }
   
-  // Analyse
+  // Analysis
   //@link : https://firefox-marketplace-api.readthedocs.org/en/latest/topics/export.html
   //@link : https://firefox-marketplace-api.readthedocs.org/en/latest/topics/apps.html#id2
   $oDirIterator = new RecursiveDirectoryIterator($dirDataTgz.$filename.'/apps');
@@ -197,56 +197,58 @@ foreach($arrayFiles as $key => $filename){
     if ($oFile->getFilename() === '.' || $oFile->getFilename() === '..' || !$oFile->isFile()) {
       continue;
     }
-	$content = trim(file_get_contents($oFile->getPathname()));
-	if($content == ''){
-		continue;
-	}
-	// Application
-	$oApp = json_decode($content);
-	
-	// Stats
-    $arrayStats['numApps'][$filename]++;
-	$arrayStats['typePremium'][$oApp->premium_type][$filename]++;
-	$arrayStats['typeApp'][$oApp->app_type][$filename]++;
-	foreach($oApp->device_types as $device_type){
-	  $arrayStats['typeDevice'][$device_type][$filename]++;
+    $content = trim(file_get_contents($oFile->getPathname()));
+    if($content == ''){
+        continue;
     }
-	$arrayStats['rating']['num'][$filename] += $oApp->ratings->count;
-	$arrayStats['rating']['amount'][$filename] += $oApp->ratings->average;
-	$arrayStats['rating']['average'][$filename] = $arrayStats['rating']['amount'][$filename] / $arrayStats['numApps'][$filename];
-	if(isset($oApp->is_disabled) && $oApp->is_disabled == true){
-		$arrayStats['stateDisabled'][1][$filename]++;
-	} else {
-		$arrayStats['stateDisabled'][0][$filename]++;
-	}
-	if(isset($oApp->is_packaged) && $oApp->is_packaged == true){
-		$arrayStats['statePackaged'][1][$filename]++;
-	} else {
-		$arrayStats['statePackaged'][0][$filename]++;
-	}
-	if(isset($oApp->public_stats) && $oApp->public_stats == true){
-		$arrayStats['statePublicStats'][1][$filename]++;
-	} else {
-		$arrayStats['statePublicStats'][0][$filename]++;
-	}
+    // Application
+    $oApp = json_decode($content);
+    
+    // Stats
+    $arrayStats['numApps'][$filename]++;
+    $arrayStats['typePremium'][$oApp->premium_type][$filename]++;
+    $arrayStats['typeApp'][$oApp->app_type][$filename]++;
+    foreach($oApp->device_types as $device_type){
+      $arrayStats['typeDevice'][$device_type][$filename]++;
+    }
+    $arrayStats['rating']['num'][$filename] += $oApp->ratings->count;
+    $arrayStats['rating']['amount'][$filename] += $oApp->ratings->average;
+    $arrayStats['rating']['average'][$filename] = $arrayStats['rating']['amount'][$filename] / $arrayStats['numApps'][$filename];
+    if(isset($oApp->is_disabled) && $oApp->is_disabled == true){
+        $arrayStats['stateDisabled'][1][$filename]++;
+    } else {
+        $arrayStats['stateDisabled'][0][$filename]++;
+    }
+    if(isset($oApp->is_packaged) && $oApp->is_packaged == true){
+        $arrayStats['statePackaged'][1][$filename]++;
+    } else {
+        $arrayStats['statePackaged'][0][$filename]++;
+    }
+    if(isset($oApp->public_stats) && $oApp->public_stats == true){
+        $arrayStats['statePublicStats'][1][$filename]++;
+    } else {
+        $arrayStats['statePublicStats'][0][$filename]++;
+    }
 
-	if($bLast && !empty($oApp->created)){
-		// Creation date
-		$dateCreated = substr($oApp->created, 0, 7);
-		if(!isset($arrayStats['lastMonth']['createdDate'][$dateCreated.'-01'])){
-			$arrayStats['lastMonth']['createdDate'][$dateCreated.'-01'] = 0;
-		}
-		$arrayStats['lastMonth']['createdDate'][$dateCreated.'-01']++;
-		// Price
-		if(!is_null($oApp->price_locale)){
-			$oApp->price_locale = str_replace('$', '', $oApp->price_locale);
-			if(!isset($arrayStats['lastMonth']['priceLocale'][$oApp->price_locale])){
-				$arrayStats['lastMonth']['priceLocale'][$oApp->price_locale] = 0;
-			}
-			$arrayStats['lastMonth']['priceLocale'][$oApp->price_locale]++;
-		}		
-		// Rating
-		if($oApp->ratings->average >= 5){
+    if($bLast){
+        // Creation date
+        if(!empty($oApp->created)){
+            $dateCreated = substr($oApp->created, 0, 7);
+            if(!isset($arrayStats['lastMonth']['createdDate'][$dateCreated.'-01'])){
+                $arrayStats['lastMonth']['createdDate'][$dateCreated.'-01'] = 0;
+            }
+            $arrayStats['lastMonth']['createdDate'][$dateCreated.'-01']++;
+        }
+        // Price
+        if(!is_null($oApp->price_locale)){
+            $oApp->price_locale = str_replace('$', '', $oApp->price_locale);
+            if(!isset($arrayStats['lastMonth']['priceLocale'][$oApp->price_locale])){
+                $arrayStats['lastMonth']['priceLocale'][$oApp->price_locale] = 0;
+            }
+            $arrayStats['lastMonth']['priceLocale'][$oApp->price_locale]++;
+        }        
+        // Rating
+        if($oApp->ratings->average >= 5){
           $arrayStats['lastMonth']['avgRating5']++;
         }
         if($oApp->ratings->average >= 4 && $oApp->ratings->average < 5){
@@ -264,30 +266,30 @@ foreach($arrayFiles as $key => $filename){
         if($oApp->ratings->average >= 0 && $oApp->ratings->average < 1){
           $arrayStats['lastMonth']['avgRating0']++;
         }
-		// Locale
-		if(isset($arrayGeo[$oApp->default_locale])){
-			if(!isset($arrayStats['lastMonth']['geo'][$arrayGeo[$oApp->default_locale]])){
-				$arrayStats['lastMonth']['geo'][$arrayGeo[$oApp->default_locale]] = 0;
-			}
-			$arrayStats['lastMonth']['geo'][$arrayGeo[$oApp->default_locale]]++;
-		} else {
-			echo $oApp->default_locale.PHP_EOL;
-		}
-		// Filesize
-		$before = 0;
-		foreach($arrayStats['lastMonth']['size'] as $keySize => $numSize){
-			if($before < $oApp->file_size && $keySize >= $oApp->file_size){
-				$arrayStats['lastMonth']['size'][$keySize]++;
-				break;
-			}
-			$before = $keySize;
-		}
-	}
+        // Locale
+        if(isset($arrayGeo[$oApp->default_locale])){
+            if(!isset($arrayStats['lastMonth']['geo'][$arrayGeo[$oApp->default_locale]])){
+                $arrayStats['lastMonth']['geo'][$arrayGeo[$oApp->default_locale]] = 0;
+            }
+            $arrayStats['lastMonth']['geo'][$arrayGeo[$oApp->default_locale]]++;
+        } else {
+            echo $oApp->default_locale.PHP_EOL;
+        }
+        // Filesize
+        $before = 0;
+        foreach($arrayStats['lastMonth']['size'] as $keySize => $numSize){
+            if($before < $oApp->file_size && $keySize >= $oApp->file_size){
+                $arrayStats['lastMonth']['size'][$keySize]++;
+                break;
+            }
+            $before = $keySize;
+        }
+    }
   }
   unset($oIterator);
   unset($oDirIterator);
 
-  // Nettoyage
+  // Cleaning
   if(file_exists($dirDataTgz.$filename.'.tar')){
     unlink($dirDataTgz.$filename.'.tar');
     fnSystem_rmdir($dirDataTgz.$filename);
